22457596b7c336b26eece7e194dad5397dc7ede5
ZhuShaoHua <zhushaohua@inbs.cn>
2016-02-16 17:15:46 +0800

    第一次上传

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by tusm on 16/1/29.
+//  Copyright © 2016年 xingqiaoshuju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by tusm on 16/1/29.
+//  Copyright © 2016年 xingqiaoshuju. All rights reserved.
+//
+
+#import "ViewController.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+ 
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
